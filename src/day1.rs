586d8b03cbc580e4705fd465215389192847682b
TheGrand547 <sdfisher79@gmail.com>
2025-12-02 12:04:26 -0500

    Questionable chagnes

--- src/day1.rs
+++ src/day1.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::BufRead;
 use std::path::Path;
 use std::time::SystemTime;
+use std::fs::read_to_string;
 
 pub fn run(filename: &str) {
     let now = SystemTime::now();
@@ -14,54 +15,45 @@ pub fn run(filename: &str) {
     let mut lock_pos: i64 = 50;
     let mut total: i64 = 0;
     let mut advanced: i64 = 0;
-    let mut advanced2: i64 = 0;
     for mut line in lines.map_while(Result::ok) {
         let previous = lock_pos;
-        if line.len() > 0 {
-            match line.remove(0) {
-                'L' => lock_pos -= line.parse::<i64>().unwrap(),
-                'R' => lock_pos += line.parse::<i64>().unwrap(),
-                _ => panic!("Invalid"),
-            }
+        //if line.len() > 0 {
+
+        match line.remove(0) {
+            'L' => lock_pos -= line.parse::<i64>().unwrap(),
+            _ => lock_pos += line.parse::<i64>().unwrap_or(0),
         }
-        let quotient2 = lock_pos / 100;
+        //}
         let quotient = lock_pos.div_euclid(100);
         let remainder = lock_pos.rem_euclid(100);
 
-        if previous != 0 && lock_pos < 0 {
-            advanced += 1;
-        }
-        advanced += quotient2.abs();
-        advanced2 += quotient.abs();
+        advanced += quotient.abs();
+        let temp3: i64 = (remainder == 0).into();
+        let temp4: i64 = (previous != 0 && lock_pos == 0).into();
+        /*
         if remainder == 0 {
             total += 1;
-            //advanced += 1;
-            if quotient2.abs() < 1 {
-                advanced += 1;
-            }
         }
         if previous != 0 && lock_pos == 0 {
-            advanced2 += 1;
-        }
-        if previous == 0 && quotient < 0 {
-            advanced2 -= 1;
-        }
-        if quotient < 0 && remainder == 0 {
-            advanced2 += 1;
-        }
-
-        println!("{}==={}", lock_pos, previous);
-        println!("{}:{}", quotient2, quotient);
-        println!("{};{}", advanced, advanced2);
-        //assert!(advanced == advanced2);
+            advanced += 1;
+        }*/
+        let temp: i64 = (quotient < 0 && previous == 0).into();
+        let temp2: i64 = (quotient < 0 && remainder == 0).into();
+        advanced += temp2 + temp4 - temp;
+        total += temp3;
+        /*
+        if quotient < 0 {
+            if previous == 0 {
+                advanced -= 1;
+            }
+            if remainder == 0 {
+                advanced += 1;
+            }
+        }*/
         lock_pos = remainder;
-        if ((advanced - advanced2).abs() > 2) {
-            panic!("ahhh");
-        }
     }
     let elapsted = now.elapsed().unwrap().as_micros();
     println!("Combination {}", total);
     println!("Strong Combination {}", advanced);
-    println!("Strong Combination(new) {}", advanced2);
     println!("Time taken (micros) {}", elapsted);
 }
