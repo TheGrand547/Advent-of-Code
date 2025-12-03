328ea08737b84b50951ae4535166f2eb99b9218e
TheGrand547 <sdfisher79@gmail.com>
2025-12-03 15:14:49 -0500

    Bad and non-functional extra parallelism

--- src/day2.rs
+++ src/day2.rs
@@ -3,8 +3,8 @@ use std::path::Path;
 
 use std::fs::read_to_string;
 
+use std::collections::HashMap;
 use std::thread;
-
 pub fn run(filename: &str) {
     let mut invalid_total: usize = 0;
     let mut double_invalid: usize = 0;
@@ -27,13 +27,14 @@ pub fn run(filename: &str) {
                 if division.len() == 2 {
                     let first = division[0].parse::<usize>().unwrap();
                     let last = division[1].parse::<usize>().unwrap();
+                    //let mut mapping = HashMap::<usize, Vec<usize>>::new();
+
                     for i in first..=last {
                         let parsed = i.to_string();
                         let length = parsed.len();
-                        /*
                         if length <= 1 {
                             continue;
-                        }*/
+                        }
                         // Only even numbers need to be chcecked for the basic versions
 
                         if length % 2 == 0 {
@@ -44,24 +45,75 @@ pub fn run(filename: &str) {
                             }
                         }
                         let bound_check = length / 2;
-                        let raw_bytes = parsed.as_str();
+                        //let raw_bytes = parsed.as_str();
+                        /*
+                        if !mapping.contains_key(&length) {
+                            let mut screaming = Vec::new();
+                            for j in 1..=bound_check {
+                                if length % j == 0 {
+                                    screaming.push(j);
+                                }
+                            }
+                            mapping.insert(length, screaming);
+                        }
+                        */
+                        if length < 10 || true {
+                            let raw_bytes = parsed.as_str();
+                            for j in (1..=bound_check).rev() {
+                                if length % j != 0 {
+                                    continue;
+                                }
+                                let sub_string = &raw_bytes[0..j];
+                                let sub_len = sub_string.len();
+                                let mut index = sub_len;
+                                while raw_bytes[index..].find(sub_string).unwrap_or(length) == 0 {
+                                    index += sub_len;
+                                }
 
-                        for j in (1..=bound_check) {
-                            if length % j != 0 {
-                                continue;
+                                if index == length {
+                                    local_total += i;
+                                    break;
+                                }
                             }
-                            let sub_string = &raw_bytes[0..j];
-                            let mut index = sub_string.len();
-                            while raw_bytes[index..].find(sub_string).unwrap_or(length) == 0 {
-                                index += sub_string.len();
+                        } else {
+                            let mut intneral_handles = vec![];
+                            for offset in [0, 1] {
+                                let copied = parsed.clone();
+                                let local_handle = thread::spawn(move || {
+                                    let mut result = false;
+                                    for j in ((1 + offset)..=bound_check).step_by(2) {
+                                        if length % j != 0 {
+                                            continue;
+                                        }
+                                        let sub_string = &copied[0..j];
+                                        let sub_len = sub_string.len();
+                                        let mut index = sub_len;
+                                        while copied[index..].find(sub_string).unwrap_or(length)
+                                            == 0
+                                        {
+                                            index += sub_len;
+                                        }
+                                        if index == length {
+                                            result = true;
+                                            break;
+                                        }
+                                    }
+                                    result
+                                });
+                                intneral_handles.push(local_handle);
                             }
-                            if index == length {
+
+                            let mut addition = false;
+                            for thread in intneral_handles {
+                                addition |= thread.join().unwrap();
+                            }
+                            if addition {
                                 local_total += i;
-                                break;
                             }
                         }
                     }
                 }
+
                 (local_total, local_invalid)
             });
             handles.push(current_handle);
